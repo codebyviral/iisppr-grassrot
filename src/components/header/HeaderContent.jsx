9e1e9d9790e8669660ebc2ecaa55a966ce6b97cf
BHASKAR <bassnaidu1242@gmail.com>
2025-01-05 14:20:30 +0530

    Update HeaderContent.jsx

--- src/components/header/HeaderContent.jsx
+++ src/components/header/HeaderContent.jsx
@@ -4,7 +4,7 @@ function HeaderContent() {
   return (
     <div>
       <h1 className="text-4xl font-bold text-white">
-        NGO Social Media Templates
+       IISPPR NGO Social Media Templates 
       </h1>
       <p className="mt-2 text-blue-100">
         Create stunning social media posts for your organization in minutes
